5e2a326daeb5b148c5105e089fe8da7bdecb340d
JeongSeob LEE <74814652+DenebLee@users.noreply.github.com>
2022-01-11 11:07:53 +0900

    Update 23.Hough_line_1_quiz.py

--- VisualCognitive2/23.Hough_line_1_quiz.py
+++ VisualCognitive2/23.Hough_line_1_quiz.py
@@ -12,7 +12,7 @@ gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 canny = cv2.Canny(gray, 100, 350, apertureSize = 3, L2gradient = True)
 
 # lines = cv2.HoughLinesP(canny, 거리단위, 각도단위, 임계값, minLineLength, maxLineGap)
-# - 확률적으로 선태하므로, thresh 값을 작게 주어야 한다.
+# - 확률적으로 선태하므로, thresh 값을 작게 주어야 한다 ----> 중요
 # - minLineLength는 최소 선길이를 지정. 이보다 작인 선은 직선에서 제외
 # - maxLineGap은 라인 간의 최대 허용 간격을 의미. 이보다 간격이 좁은 경우 직선으로 허용하지 않음.
 # - 반환값은 (N, 1, 4)의 형태를 나타내면, 4의 각 좌표는 x1, y1, x2, y2의 직선값을 나타낸다.
